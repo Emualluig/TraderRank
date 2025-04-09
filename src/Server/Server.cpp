93e6f85f220f8824ead8ae2cc080e687b1598ed9
Emualluig <Emualluig@users.noreply.github.com>
2025-04-09 15:21:06 -0400

    Added Pybind11 Project

---
+++ src/Server/Server.cpp
@@ -0,0 +1,22 @@
+﻿// Server.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+int meaning_of_life() {
+    return 42;
+}
+
+PYBIND11_MODULE(Server, m) {
+    m.doc() = "Example pybind11 module";
+    m.def("meaning_of_life", &meaning_of_life, "Returns the meaning of life.");
+}
+
+//int main()
+//{
+//	std::cout << "Hello CMake." << std::endl;
+//	return 0;
+//}
